0a9d0c3be04e9638d6637e13a2d694b6e2f2adca
Emirhan Yıldızer <103773791+TmSmob@users.noreply.github.com>
2024-07-25 11:23:35 +0300

    Interface v1

---
+++ CSharpCourse/Classes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustamerManager custamerManager = new CustamerManager();
+            ProductManager productManager = new ProductManager();
+            custamerManager.Add();
+            custamerManager.Update();
+            productManager.Add();
+            productManager.Update();
+
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Engin";
+            customer.LastName = "Demirog";
+
+            Customer customer2 = new Customer
+            {//CTRL+Space ile cok rahat gorebilirsin.
+                Id = 2,
+                City = "Istanbul",
+                FirstName = "Emirhan",
+                LastName = "Yildizer",
+            };
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ CSharpCourse/InterFaces/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonManager personManager = new PersonManager();
+            
+
+            Students students = new Students
+            {
+                id = 1,
+                FirstName = "Batuhan",
+                LastName = "Yildizer",
+                Departmant = "HighSchool"
+            };
+            personManager.Add(students);
+            Console.ReadLine(); 
+        }
+    }
+
+    interface IPerson
+    {
+        int id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    class Customers:IPerson
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Adress { get; set; }
+    }
+
+    class Students:IPerson
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
