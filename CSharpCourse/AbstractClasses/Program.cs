d1fc5d49695ac41ddf80cc745b8a92b28f54f8bb
Emirhan Yıldızer <103773791+TmSmob@users.noreply.github.com>
2024-07-26 04:12:20 +0300

    Interfaces,Inheritances,Virtual Methods and Absturact

---
+++ CSharpCourse/AbstractClasses/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer= new SqlServer();
+            sqlServer.Add();
+            sqlServer.Delete();
+            Console.ReadLine();
+            Database database = new Database();
+        }
+        abstract class Database
+        {
+            public void Add()
+            {
+                Console.WriteLine("ADDED BY DEFAULT");
+            }
+
+            public abstract void Delete();
+            
+        }
+
+        class SqlServer:Database
+        {
+            public override void Delete()
+            {
+                 Console.WriteLine("Deleted By Default");
+            }
+        }
+    }
+}
