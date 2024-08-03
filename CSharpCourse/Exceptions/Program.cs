d0e66c1c3a80aefa20e14590815e5d06c25375b4
Emirhan Yıldızer <103773791+TmSmob@users.noreply.github.com>
2024-08-03 14:16:37 +0300

    Exceptions

--- CSharpCourse/Exceptions/Program.cs
+++ CSharpCourse/Exceptions/Program.cs
@@ -10,22 +10,81 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            //ExceptionIntro();
+            try
+            {
+                Find();
+            }
+            catch (RecordNotFoundExceptions exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+            }
+            //Method
+            HandleException(() => { Find(); });
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void HandleException(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private static void Find()
+        {
+            List<string> students = new List<string> { "Engin", "Derin", "Salih" };
+            if (!students.Contains("Ahmet"))
+            {
+                throw new RecordNotFoundExceptions("Record Not Found");
+            }
+            else
+            {
+                Console.WriteLine("Record Found");
+            }
+        }
+
+        private static void ExceptionIntro()
+        {
             try
             {
                 string[] students = new string[3] { "Engin", "Derin", "Salih" };
                 students[3] = "Ahmet";
             }
-            catch(IndexOutOfRangeException exception)
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+
+            }
+            catch (DivideByZeroException exception)
             {
                 Console.WriteLine(exception.Message);
 
             }
             catch (Exception exception)
             {
-               // Console.WriteLine(exception.Message);
+                Console.WriteLine(exception.Message);
             }
-         
-            Console.ReadLine();
         }
     }
 }
